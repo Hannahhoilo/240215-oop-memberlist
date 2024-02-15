343490c589c66078c9ee34c02a9642d35a28b61f
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-02-15 11:09:24 +0100

    Add ProMember and eventlistener

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@
 const firstnameInput = document.querySelector(".firstname");
 const lastnameInput = document.querySelector(".lastname");
 const phoneInput = document.querySelector(".phonenumber");
+const isProInput = document.querySelector(".pro-member");
 
 const submitButton = document.querySelector(".submit-button");
 
@@ -15,10 +16,35 @@ class Member {
 		this.phonenumber = phonenumber
 	}
 
-	createMember() {
+	static createMember() {
 		const newMember = new Member(firstnameInput.value, lastnameInput.value, phoneInput.value);
 		return newMember;
 	}
 }
+
+class ProMember extends Member {
+	constructor(firstname, lastname, phonenumber, isProMember) {
+		super(firstname, lastname, phonenumber)
+		this.isProMember = isProMember
+	}
+
+	static createProMember() {
+		const newProMember = new ProMember(firstnameInput.value, lastnameInput.value, phoneInput.value, isProInput.checked);
+		return newProMember;
+	}
+}
+
+submitButton.addEventListener("click", (event)=> {
+	event.preventDefault();
+	if(isProInput.checked) {
+		allMembers.push(ProMember.createProMember());
+	} else {
+		allMembers.push(Member.createMember());
+	}
+	console.log(allMembers);
+});
+
+
+
 
 
