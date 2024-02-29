f70be19a1e4538196c36a4e88b9fb0f152728a05
Hannah Høilo <143997275+Hannahhoilo@users.noreply.github.com>
2024-02-29 10:48:04 +0100

    added stuff

--- src/js/app.js
+++ src/js/app.js
@@ -15,11 +15,20 @@ class Member {
 		this.firstname = firstname
 		this.lastname = lastname
 		this.phonenumber = phonenumber
+		this.id = Date.now()
 	}
 
 	static createMember() {
 		const newMember = new Member(firstnameInput.value, lastnameInput.value, phoneInput.value);
 		return newMember;
+	}
+
+	static removeMember(id, list) {
+		const index = list.findIndex((member) => member.id.toString() === id.toString());
+		if(index !== -1) {
+			list.splice(index, 1);
+			UI.renderList();
+		}
 	}
 }
 
@@ -54,16 +63,24 @@ class UI {
 		memberList.textContent = "";
 		allMembers.forEach((member)=> {
 			const memberLi = document.createElement("li");
+			memberLi.dataset.id = member.id;
 			memberList.appendChild(memberLi);
 			memberLi.textContent = member.firstname + member.lastname;	
+			const deleteButton = document.createElement("button")
+			deleteButton.textContent = "Delete"
+			memberLi.append(deleteButton)
 				
 			if(member.isProMember) {
 				memberLi.classList.add("pro-bold");
 			}
+			deleteButton.addEventListener('click', (event)=> {
+				event.preventDefault();
+				const currentId = event.currentTarget.parentElement.dataset.id;
+				console.log();
+				Member.removeMember(currentId, allMembers)
+			})
 		});
 
 	}
 }
 
-
-allMembers
